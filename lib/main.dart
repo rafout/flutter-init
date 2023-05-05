ba0ee983a10e81eec1f9552f081a592db367816b
Rafael Andrade <rafael@mgcode.com.br>
2023-05-05 10:54:06 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(Container(
+    child: Text('Novo app Flutter.',
+        textDirection: TextDirection.ltr,
+        style: TextStyle(color: Colors.white)),
+  ));
+}
