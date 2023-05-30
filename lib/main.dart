4fb8cf024e78df3b3f5aae91552112647a6faaf3
Rafael Andrade <rafael@mgcode.com.br>
2023-05-29 21:03:06 -0300

    adicionando gifs para escuta e fala

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:avatar_glow/avatar_glow.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_gif/flutter_gif.dart';
 import 'package:highlight_text/highlight_text.dart';
 import 'package:speech_to_text/speech_to_text.dart' as stt;
 import 'package:flutter_tts/flutter_tts.dart';
@@ -36,75 +35,31 @@ class SpeechScreen extends StatefulWidget {
 class SpeechScreenState extends State<SpeechScreen>
     with TickerProviderStateMixin {
   final FlutterTts flutterTts = FlutterTts();
-  late FlutterGifController controller;
 
   final apiUrl = 'https://api.openai.com/v1/chat/completions';
-
-  final Map<String, HighlightedWord> _highlights = {
-    'flutter': HighlightedWord(
-      onTap: () => print('flutter'),
-      textStyle: const TextStyle(
-        color: Colors.blue,
-        fontWeight: FontWeight.bold,
-      ),
-    ),
-    'voice': HighlightedWord(
-      onTap: () => print('voice'),
-      textStyle: const TextStyle(
-        color: Colors.green,
-        fontWeight: FontWeight.bold,
-      ),
-    ),
-    'subscribe': HighlightedWord(
-      onTap: () => print('subscribe'),
-      textStyle: const TextStyle(
-        color: Colors.red,
-        fontWeight: FontWeight.bold,
-      ),
-    ),
-    'like': HighlightedWord(
-      onTap: () => print('like'),
-      textStyle: const TextStyle(
-        color: Colors.blueAccent,
-        fontWeight: FontWeight.bold,
-      ),
-    ),
-    'comment': HighlightedWord(
-      onTap: () => print('comment'),
-      textStyle: const TextStyle(
-        color: Colors.green,
-        fontWeight: FontWeight.bold,
-      ),
-    ),
-  };
 
   late stt.SpeechToText _speech;
   bool _isListening = false;
   bool _isSpeaking = false;
   String _text = 'Olá, o que você deseja?';
   String _voice = '';
-  double _confidence = 1.0;
 
   @override
   void initState() {
     super.initState();
-    controller = FlutterGifController(vsync: this);
     _speech = stt.SpeechToText();
     _startInteraction();
-  }
-
-  @override
-  void dispose() {
-    controller.dispose();
-    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Confidence: ${(_confidence * 100.0).toStringAsFixed(1)}%'),
+        title: const Text('Asimov GPT'),
+        backgroundColor: Colors.blue[600],
+        centerTitle: true,
       ),
+      backgroundColor: Colors.black,
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       floatingActionButton: Visibility(
         visible: !_isSpeaking,
@@ -123,59 +78,66 @@ class SpeechScreenState extends State<SpeechScreen>
               ),
             ),
             AvatarGlow(
+              endRadius: 75.0,
               child: FloatingActionButton(
                 onPressed: _play,
-                child: Icon(Icons.play_arrow),
+                child: const Icon(Icons.play_arrow),
               ),
-              endRadius: 75.0,
             ),
           ],
         ),
       ),
-      body: SingleChildScrollView(
-        reverse: true,
-        child: Column(
-          children: [
-            Container(
-              padding: const EdgeInsets.fromLTRB(30.0, 30.0, 30.0, 150.0),
-              child: TextHighlight(
-                text: _text,
-                words: _highlights,
-                textStyle: const TextStyle(
-                  fontSize: 28.0,
-                  color: Colors.black,
-                  fontWeight: FontWeight.w400,
+      body: SizedBox(
+        height: double.infinity,
+        width: double.infinity,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Visibility(
+                visible: _isSpeaking,
+                child: const Image(
+                  image: AssetImage("assets/talking.gif"),
                 ),
               ),
-            ),
-            Visibility(
-              visible: _isSpeaking,
-              child: GifImage(
-                controller: controller,
-                image: const AssetImage("assets/giphy.gif"),
+              Visibility(
+                visible: _isListening,
+                child: const Image(
+                  image: AssetImage("assets/listening.gif"),
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
   }
 
   Future<void> _startInteraction() async {
+    setState(() {
+      _isSpeaking = true;
+      _isListening = false;
+    });
+
     await Future.delayed(const Duration(seconds: 1));
     await flutterTts.setLanguage('pt-BR');
-    await flutterTts.speak('Olá, o que você deseja?');
+    await flutterTts.speak(_text);
+
+    flutterTts.setCompletionHandler(() {
+      setState(() {
+        _isSpeaking = false;
+      });
+    });
+
     await Future.delayed(const Duration(seconds: 3));
     _listen();
   }
 
   void _listen() async {
-    print(_isListening);
     if (!_isListening) {
       bool available = await _speech.initialize(
         onStatus: (val) => print('onStatus: $val'),
         onError: (val) {
-          print('onError: ${val.errorMsg}');
           if (val.errorMsg == 'error_speech_timeout' ||
               val.errorMsg == 'error_no_match') {
             _startInteraction();
@@ -185,13 +147,9 @@ class SpeechScreenState extends State<SpeechScreen>
       if (available) {
         setState(() => _isListening = true);
         _speech.listen(
-          listenFor: const Duration(minutes: 3),
           onResult: (val) async {
             setState(() {
               _text = val.recognizedWords;
-              if (val.hasConfidenceRating && val.confidence > 0) {
-                _confidence = val.confidence;
-              }
             });
           },
         );
@@ -206,22 +164,20 @@ class SpeechScreenState extends State<SpeechScreen>
     _voice = await getChatGptResponse(_text);
 
     setState(() {
+      _isListening = false;
       _isSpeaking = true;
     });
 
-    controller.repeat(
-      min: 0,
-      max: 3,
-      period: const Duration(milliseconds: 500),
-    );
-
     await flutterTts.setLanguage('pt-BR');
-    await flutterTts.speak(_voice);
+    await flutterTts.speak(_text);
 
     flutterTts.setCompletionHandler(() {
-      setState(() {
+      setState(() async {
         _isSpeaking = false;
-        controller.reset();
+        _isListening = true;
+        await Future.delayed(const Duration(seconds: 3));
+        _text = 'Posso te ajudar em algo mais?';
+        _startInteraction();
       });
     });
   }
@@ -240,7 +196,7 @@ class SpeechScreenState extends State<SpeechScreen>
       ],
       'max_tokens': 50,
     };
-    print(Uri.parse(apiUrl));
+
     final response = await http.post(Uri.parse(apiUrl),
         headers: headers, body: jsonEncode(data));
 
